1dd0ae7db1bd55d47c14a4643e04dbbfe2f957c6
abhi <abhigowdabs154@gmail.com>
2024-10-06 22:26:05 +0530

    create readme

---
+++ README.md
@@ -0,0 +1,23 @@
+## Installation
+
+1. **Clone the repository**:
+    ```bash
+    https://github.com/abhi-gowdaa/food-recipes-frontend.git
+    cd recipe-frontend
+    ```
+
+2. **Install dependencies**:
+    ```bash
+    npm install
+    ```
+
+3. **Start the development server**:
+    ```bash
+    npm start
+    ```
+
+4. **Build for production** (optional):
+    ```bash
+    npm run build
+    ```
+
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,32 @@
-import MainNavigation from "../components/MainNavigation"
+import { MainNavigation } from "../components/MainNavigation"
+import { Box } from "@mui/material" 
+
 
 export function Error(){
 
     return <>
     <MainNavigation/>
     <main>
+    <Box
+    className="data-card"
+      component="section"
+      sx={{
+        p: 2,
+        m: "auto",
+        mt: "50px",
+        mb:"10px",
+        maxWidth: "60rem",
+        height: "500px",
+        overflow:"hidden",
+        borderRadius: "25px",
+        bgcolor:"#fff",
+        textAlign:"center",
+       justifyContent:"center"
+      }}
+    >
         <h1>An Error Occured</h1>
         <p>could not find this page!</p>
+        </Box>
     </main>
     </>
 }
